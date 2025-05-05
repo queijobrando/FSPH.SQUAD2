8038aab8d3ea0adb020c0cab66589ba8b8b0d264
Tiago Souza <tiagoadesouza21@gmail.com>
2025-05-05 14:07:52 -0300

    Criando arquivos

--- src/main/java/com/squad2/fsph/FSPH/SQUAD2/amostra/model/Amostra.java
+++ src/main/java/com/squad2/fsph/FSPH/SQUAD2/amostra/model/Amostra.java
@@ -1,6 +1,13 @@
 package com.squad2.fsph.FSPH.SQUAD2.amostra.model;
 
+
+import java.time.LocalDateTime;
+
 public class Amostra {
+
+    private Long id;
+    private LocalDateTime dataHora;
+    private Endereco endereco;
 
 
 }
---
+++ src/main/java/com/squad2/fsph/FSPH/SQUAD2/amostra/model/Endereco.java
@@ -0,0 +1,10 @@
+package com.squad2.fsph.FSPH.SQUAD2.amostra.model;
+
+public class Endereco {
+    private String logradouro;
+    private String bairro;
+    private String cep;
+    private String numero;
+    private String complemento;
+    private String municipio;
+}
